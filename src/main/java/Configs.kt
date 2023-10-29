ad436ffdc1eacaa369d878253806a6c23afc005e
JeyK <s.merlinjeyakumar@gmail.com>
2023-10-29 13:02:47 +0530

    package change comments update

--- src/main/java/Configs.kt
+++ src/main/java/Configs.kt
@@ -1,5 +1,5 @@
 object Configs {
-    const val applicationId = "com.nativedevps.myapplication"
+    const val applicationId = "com.nativedevps.arch"
     const val kotlinVersion = "1.7.20"
     const val classpathGradleVersion = "7.5"
     const val kotlinGradlePlugin = "1.8.20"
